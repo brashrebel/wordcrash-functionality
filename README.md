6bcb78ca74f3d84969b57ef72dd84efb4ae9a551
Kyle Maurer <kyle@realbigmarketing.com>
2015-08-25 13:24:32 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+### WordCrash Functionality
+
+This WordPress plugin adds some functionality to the WordCrash website so it can be usable.
+
+Contributions are welcome.
---
+++ wordcrash-functionality.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: WordCrash Functionality
+Plugin URI: http://github.com/brashrebel/wordcrash-functionality
+Description: Adds the necessary functionality to the WordCrash website.
+Version: 0.1
+Author: WP Ann Arbor
+Author URI: http://wpannarbor.com
+License: GPL2
+*/
+
