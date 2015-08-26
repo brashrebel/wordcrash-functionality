20b36bb320702ed027fdd8c7ad9cb4be381e772a
Kyle Maurer <kyle@realbigmarketing.com>
2015-08-26 19:11:57 -0400

    Added the user id to email function.

--- wordcrash-functionality.php
+++ wordcrash-functionality.php
@@ -9,6 +9,16 @@ Author URI: http://wpannarbor.com
 License: GPL2
 */
 
+/**
+ * Convert the hidden field's value from ID to email address
+ * First number in filter name is form ID, second is field ID
+ */
+add_filter( 'gform_get_input_value_2_4', 'wc_host_id_to_email', 10 );
+function wc_host_id_to_email( $value ) {
+	$email = get_userdata( $value );
+	$email = $email->user_email;
+	return $email;
+}
 
 /**
  * Add pets and capacity user meta fields
