d94370f202f99d959c1d9cc7271760f58da26532
Eric Defore <eric.defore@gmail.com>
2016-08-23 09:14:10 -0400

    Force uniformity of submitted Countries and States. Closes brashrebel/wordcrash-theme#20

--- wordcrash-functionality.php
+++ wordcrash-functionality.php
@@ -81,9 +81,116 @@ function wc_save_new_user_fields( $user_id ) {
 
 	if ( !current_user_can( 'edit_user', $user_id ) )
 		return false;
+    
+    $states = wc_get_states_array();
+    
+    if ( array_key_exists( strtoupper( $_POST['state'] ), $states ) ) {
+            
+        $_POST['state'] = $states[ strtoupper( $_POST['state'] ) ];
+
+    }
+    
+    $_POST['country'] = preg_replace( '/(?:The\s)?United\sStates(?:\sof\sAmerica)?/i', 'USA', $_POST['country'] );
+    if ( strtolower( $_POST['country'] ) == 'us' || $_POST['country'] == 'usa' ) $_POST['country'] = 'USA'; // To grab those outliers
+    
 	update_user_meta( $user_id, 'country', $_POST['country'] );
 	update_user_meta( $user_id, 'state', $_POST['state'] );
 	update_user_meta( $user_id, 'city', $_POST['city'] );
 	update_user_meta( $user_id, 'pets', $_POST['pets'] );
 	update_user_meta( $user_id, 'capacity', $_POST['capacity'] );
+    
+}
+
+// Ensure uniformity for USA and States
+add_action( 'gform_pre_submission_2', 'wc_sanitize_countries_states' );
+function wc_sanitize_countries_states( $form ) {
+    
+    $states = wc_get_states_array();
+    
+    foreach ( $form['fields'] as &$field ) {
+        
+        // Skip to the next one if it isn't what we want
+        if ( $field->label !== 'State/Province' && $field->label !== 'Country' ) {
+            continue;
+        }
+        
+        if ( $field->label == 'State/Province' ) {
+            
+            if ( array_key_exists( strtoupper( $_POST[ 'input_' . $field->id ] ) ) ) {
+            
+                $_POST[ 'input_' . $field->id ] = $states[ strtoupper( $_POST[ 'input_' . $field->id ] ) ];
+                
+            }
+            
+        }
+        else if ( $field->label == 'Country' ) {
+            
+            $_POST[ 'input_' . $field->id ] = preg_replace( '/(?:The\s)?United\sStates(?:\sof\sAmerica)?/i', 'USA', $_POST[ 'input_' . $field->id ] );
+            if ( strtolower( $_POST[ 'input_' . $field->id ] ) == 'us' || $_POST[ 'input_' . $field->id ] == 'usa' ) $_POST[ 'input_' . $field->id ] = 'USA'; // To grab those outliers
+            
+        }
+        
+    }
+    
+}
+
+// No sense in having this twice
+function wc_get_states_array() {
+    
+    $states = array(
+        'AL' => 'Alabama',
+        'AK' => 'Alaska',
+        'AZ' => 'Arizona',
+        'AR' => 'Arkansas',
+        'CA' => 'California',
+        'CO' => 'Colorado',
+        'CT' => 'Connecticut',
+        'DE' => 'Delaware',
+        'DC' => 'District of Columbia',
+        'FL' => 'Florida',
+        'GA' => 'Georgia',
+        'HI' => 'Hawaii',
+        'ID' => 'Idaho',
+        'IL' => 'Illinois',
+        'IN' => 'Indiana',
+        'IA' => 'Iowa',
+        'KS' => 'Kansas',
+        'KY' => 'Kentucky',
+        'LA' => 'Louisiana',
+        'ME' => 'Maine',
+        'MD' => 'Maryland',
+        'MA' => 'Massachusetts',
+        'MI' => 'Michigan',
+        'MN' => 'Minnesota',
+        'MS' => 'Mississippi',
+        'MO' => 'Missouri',
+        'MT' => 'Montana',
+        'NE' => 'Nebraska',
+        'NV' => 'Nevada',
+        'NH' => 'New Hampshire',
+        'NJ' => 'New Jersey',
+        'NM' => 'New Mexico',
+        'NY' => 'New York',
+        'NC' => 'North Carolina',
+        'ND' => 'North Dakota',
+        'OH' => 'Ohio',
+        'OK' => 'Oklahoma',
+        'OR' => 'Oregon',
+        'PA' => 'Pennsylvania',
+        'RI' => 'Rhode Island',
+        'SC' => 'South Carolina',
+        'SD' => 'South Dakota',
+        'TN' => 'Tennessee',
+        'TX' => 'Texas',
+        'UT' => 'Utah',
+        'VT' => 'Vermont',
+        'VA' => 'Virginia',
+        'WA' => 'Washington',
+        'WV' => 'West Virginia',
+        'WI' => 'Wisconsin',
+        'WY' => 'Wyoming',
+    );
+    
+    return $states;
+    
 }
